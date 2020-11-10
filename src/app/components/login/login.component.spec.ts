ffae3f66e70a8e1f6ac7a437f8eb40aafa8f8d17
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2020-11-09 21:57:58 -0400

    [Fixed] tests

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +10,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ LoginComponent ],
+      providers: [FormBuilder]
     })
     .compileComponents();
   }));
